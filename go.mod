24f6ecd142147cec7b0bacbf8474227c7d352464
shakoor123 <shakoorchachu2001@gmail.com>
2022-12-20 22:52:36 +0530

    bcrypt used for password hashing

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.19
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
 )
--- middleware/auth.go
+++ middleware/auth.go
@@ -25,11 +25,17 @@ func LoginUser(w http.ResponseWriter,r *http.Request){
 	result.Scan( &user.Uid, &user.Username,&user.Password,&user.Email)
 	}
 
-	if user.Password == u.Password{
-	json.NewEncoder(w).Encode(user)
+	// if user.Password == u.Password{
+	// json.NewEncoder(w).Encode(user)
 
+	// }else{
+	// json.NewEncoder(w).Encode("Password is incurrect")
+	// }
+	flag:=CheckPasswordHash(u.Password,user.Password)
+	if flag==true{
+		json.NewEncoder(w).Encode(user)
 	}else{
-	json.NewEncoder(w).Encode("Password is incurrect")
+		json.NewEncoder(w).Encode("Password is incurrect")
 	}
 
 }
@@ -38,7 +44,8 @@ func RegisterUSer(w http.ResponseWriter,r *http.Request){
 	u:=models.User{}
 	dec := json.NewDecoder(r.Body)
 	dec.Decode(&u);
-		_, err := config.DB.Exec(`INSERT INTO users (username,password,email) VALUES (?, ?,?)`, u.Username, u.Password,u.Email)
+	hash,_:=HashPassword(u.Password)
+	_, err := config.DB.Exec(`INSERT INTO users (username,password,email) VALUES (?, ?,?)`, u.Username,hash,u.Email)
 	if err != nil {
 		log.Fatal("not inserted",err)
 	}else{
